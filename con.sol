45e3be6e791ea7abeabd1738004021234ff19f0d
Loveleen Kaur <126374845+leenubhagtana@users.noreply.github.com>
2024-02-03 17:45:49 +0530

    Update const.sol

--- con.sol
+++ con.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity  >=0.8.0.;
 
+contract const {
+    address public owner;
+    uint public balance;
+
+    constructor (uint _bal)  {
+        owner=msg.sender;
+        balance=_bal;
+    }
+}
+
+contract derived is const {
+    constructor () const(20){}
+}
