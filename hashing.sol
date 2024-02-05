7b7de18b15b95338574998e60dd4dc0e809b8796
Loveleen Kaur <126374845+leenubhagtana@users.noreply.github.com>
2024-02-05 17:41:27 +0530

    Update hashing.sol

--- hashing.sol
+++ hashing.sol
@@ -2,9 +2,15 @@
 pragma solidity >=0.8.0;
 
 contract hash {
-
+    //one way
     function gen (string memory _pass) public pure returns (bytes32) {
         return keccak256(abi.encodePacked(_pass));
 
     }
+    // we can also give 2 arguments..
+    //function gen (string memory _pass, string memory _name ) public pure returns (bytes32) {
+     //   return keccak256(abi.encodePacked(_pass,_name ));
+
+    //}
+}
 }
