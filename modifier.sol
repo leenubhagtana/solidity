44e528a5ebe121ca60d92bb70e2d424bf0100d60
Loveleen Kaur <126374845+leenubhagtana@users.noreply.github.com>
2024-02-05 15:57:54 +0530

    Update modifier.sol

--- modifier.sol
+++ modifier.sol
@@ -1,1 +1,21 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity  >=0.8.0;
+
+contract mod {
+    address public owner;
+
+    constructor() {
+        owner=msg.sender;
+    }
+
+    // Modifier Definition..they are used to define the prerequisite for calling a function ..so that we dont need to define it across multiple functions 
+    modifier onlyOwner {
+        require(owner==msg.sender);
+        _;
+    }
+
+    function test () public onlyOwner {
+        
+    }
+}
