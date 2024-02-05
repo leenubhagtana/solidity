943b3bb1707ad6d892965f4e120bab48108c9815
Loveleen Kaur <126374845+leenubhagtana@users.noreply.github.com>
2024-02-05 13:33:27 +0530

    Update payable.sol

--- payable.sol
+++ payable.sol
@@ -1,1 +1,15 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+ 
+ contract payables {
+    address payable public owner;
+
+    constructor() {
+        owner = payable (msg.sender);
+    }
+
+    function trans() payable public {
+        owner.transfer(msg.value);
+    }
+ }
