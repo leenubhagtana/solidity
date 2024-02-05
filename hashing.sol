c946a227d9ff575416602ccbbc058614806029e7
Loveleen Kaur <126374845+leenubhagtana@users.noreply.github.com>
2024-02-05 17:42:52 +0530

    Update hashing.sol

--- hashing.sol
+++ hashing.sol
@@ -4,13 +4,13 @@ pragma solidity >=0.8.0;
 contract hash {
     //one way
     function gen (string memory _pass) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(_pass));
-
+        return keccak256(abi.encode(_pass));
     }
     // we can also give 2 arguments..
     //function gen (string memory _pass, string memory _name ) public pure returns (bytes32) {
-     //   return keccak256(abi.encodePacked(_pass,_name ));
+     //   return keccak256(abi.encode(_pass,_name ));
 
     //}
+    // we also have abi.encodePacked() but issue is the hash of two inputs aaa,bbb and aaab,bb will be same .
 }
 }
