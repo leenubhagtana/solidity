3b8163ae1417e189c46ceeafdbc74d01709ef1da
Loveleen Kaur <126374845+leenubhagtana@users.noreply.github.com>
2024-02-06 12:26:52 +0530

    Create import.sol

---
+++ import.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+import "enum.sol";
+import "https://github.com/leenubhagtana/solidity/blob/main/arguments.sol";
+
+pragma solidity >=0.8.0;
+
+contract a {
+    uint public num;
+
+    
+}
+
+contract b is test,a, one {
+
+    
+}
