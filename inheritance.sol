3db9fb09dd6bde71a88e745f11512888be018231
Loveleen Kaur <126374845+leenubhagtana@users.noreply.github.com>
2024-02-06 12:24:17 +0530

    Update inheritance.sol

--- inheritance.sol
+++ inheritance.sol
@@ -1,1 +1,22 @@
 
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0;
+
+contract a {
+    uint public num;
+
+    constructor(uint _num) {
+        num=_num;
+    }
+}
+
+contract b is a(4) {
+
+    // constructor () a(4) {}
+
+    function test () public view returns (uint ) {
+        return num;
+    }
+
+}
