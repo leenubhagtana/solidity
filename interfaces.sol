b4b4eac53a69df33fb4b72e053b58a317bdd9344
Loveleen Kaur <126374845+leenubhagtana@users.noreply.github.com>
2024-02-07 18:10:00 +0530

    Update interfaces.sol

--- interfaces.sol
+++ interfaces.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
 
+// Interface
+
+// cannot have any functions implemented
+// can inherit from other interfaces
+// all declared functions must be external and by default are virtual
+// cannot declare a constructor
+// cannot declare state variables
+
+interface base {
+    function val() pure external returns(uint) ;
+}
+
+contract main is base {
+    function val() public override pure returns(uint) {
+        return 1;
+    }
+}
